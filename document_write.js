6a38dd0363861dfdcbab74d9bc53fa0f0a84875f
Cynthia London <cynthe17@gmail.com>
2017-11-01 17:05:40 -0500

    document_write

---
+++ document_write.js
@@ -0,0 +1,16 @@
+var headingtext="<h1>A Page of JavaScript</h1>";
+var myintro="Hello, welcome to my Javascript page!";
+var linktag="<a href=\"http://www.cnn.com\">Link to a Site</a>";
+var redtext="<span style=\"color:aquamarine\">I am so colorful today!</span>";
+var begineffect="<em>";
+var endeffect="</em>";
+var beginpara="<p>";
+var endpara="</p>";
+
+document.write(headingtext);
+document.write(begineffect+myintro+endeffect);
+document.write(beginpara);
+document.write(linktag);
+document.write(endpara);
+document.write(redtext);
+document.write(endpara);
